1f1f66220ea53e5a8aab38ff7ace9b1a9e53a121
b1ackitty <jieunj0301@gmail.com>
2025-07-30 13:26:15 +0900

    [feat] 악보 선택 모달 모듈화
    - constant 변수 모음
    - localStorage에 업로드한 악보 관리하는 기능 모음
    - 이벤트 핸들러
    - 악보 선택 카드 렌더

---
+++ src/components/common/music-sheet-select/constants.js
@@ -0,0 +1,34 @@
+// localStorage에 저장할 키 값
+export const LOCAL_STORAGE_KEY = "userMusicSheets";
+
+// 기본 제공 악보 데이터
+export const DEFAULT_MUSIC_SHEETS = [
+  {
+    id: "default-1",
+    name: "airplane",
+    src: "/assets/images/airplane.png",
+    type: "image",
+    isDeletable: false,
+  },
+  {
+    id: "default-2",
+    name: "butterfly",
+    src: "/assets/images/butterfly.png",
+    type: "image",
+    isDeletable: false,
+  },
+  {
+    id: "default-3",
+    name: "little-star",
+    src: "/assets/pdf/little-star.pdf",
+    type: "pdf",
+    isDeletable: false,
+  },
+  {
+    id: "default-4",
+    name: "three-bears",
+    src: "/assets/images/three-bears.png",
+    type: "image",
+    isDeletable: false,
+  },
+];
